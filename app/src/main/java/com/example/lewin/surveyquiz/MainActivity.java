4698da8e44433ceeb7c4c927e232d95cc31cdfb8
DaCodaHUB <dannylpov@gmail.com>
2018-07-27 15:27:41 -0700

    created basic UI

--- app/src/main/java/com/example/lewin/surveyquiz/MainActivity.java
+++ app/src/main/java/com/example/lewin/surveyquiz/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.lewin.surveyquiz;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+
+import com.example.lewin.surveyquiz.Fragments.StartFragment.StartFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up Fragment manager and fragment transaction
+        FragmentManager frag_manager = getSupportFragmentManager();
+        FragmentTransaction frag_transaction = frag_manager.beginTransaction();
+
+        // Put the third fragment to the screen
+        frag_transaction.replace(R.id.frame, new StartFragment());
+        frag_transaction.commit();
+
     }
 }
