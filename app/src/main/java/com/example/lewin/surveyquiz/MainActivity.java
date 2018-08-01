4dd5e1531f5e5e7d8ec07fe284682274252e49f5
DaCodaHUB <dannylpov@gmail.com>
2018-07-31 18:45:15 -0700

    Still trying to figure out how to use Dagger with Fragments

--- app/src/main/java/com/example/lewin/surveyquiz/MainActivity.java
+++ app/src/main/java/com/example/lewin/surveyquiz/MainActivity.java
@@ -5,22 +5,45 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 
+//import com.mindorks.example.android_dagger2_example.data.DataManager;
+//import com.mindorks.example.android_dagger2_example.data.model.User;
+import com.example.lewin.surveyquiz.di.components.ActivityComponent;
+import com.example.lewin.surveyquiz.di.components.DaggerActivityComponent;
+import com.example.lewin.surveyquiz.di.module.ActivityModule;
+
 import com.example.lewin.surveyquiz.Fragments.StartFragment.StartFragment;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityComponent activityComponent;
+    private FragmentTransaction fragmentTransaction;
+
+    public ActivityComponent getActivityComponent() {
+        if (activityComponent == null) {
+            activityComponent = DaggerActivityComponent.builder()
+                    .activityModule(new ActivityModule(this))
+                    .applicationComponent(SurveyApplication.get(this).getComponent())
+                    .build();
+        }
+        return activityComponent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getActivityComponent().inject(this);
+
         // Set up Fragment manager and fragment transaction
-        FragmentManager frag_manager = getSupportFragmentManager();
-        FragmentTransaction frag_transaction = frag_manager.beginTransaction();
+        //FragmentManager frag_manager = getSupportFragmentManager();
+        //FragmentTransaction frag_transaction = frag_manager.beginTransaction();
 
         // Put the third fragment to the screen
-        frag_transaction.replace(R.id.frame, new StartFragment());
-        frag_transaction.commit();
+        fragmentTransaction.replace(R.id.frame, new StartFragment());
+        fragmentTransaction.commit();
 
     }
 }
--- app/src/main/java/com/example/lewin/surveyquiz/di/module/ActivityModule.java
+++ app/src/main/java/com/example/lewin/surveyquiz/di/module/ActivityModule.java
@@ -2,12 +2,10 @@ package com.example.lewin.surveyquiz.di.module;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
-import com.example.lewin.surveyquiz.MainActivity;
 import com.example.lewin.surveyquiz.di.ActivityContext;
 
 import javax.inject.Singleton;
