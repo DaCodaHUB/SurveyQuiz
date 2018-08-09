06b980fdf2faaa29ea2de29454f2fe763a0e8d31
DaCodaHUB <dannylpov@gmail.com>
2018-08-08 18:34:53 -0700

    Add Nullable

--- app/src/main/java/com/example/lewin/surveyquiz/Fragments/ResultFragment/ResultFragment.java
+++ app/src/main/java/com/example/lewin/surveyquiz/Fragments/ResultFragment/ResultFragment.java
@@ -1,6 +1,7 @@
 package com.example.lewin.surveyquiz.Fragments.ResultFragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +20,7 @@ public final class ResultFragment extends BaseViewFragment<ResultPresenter>
     TextView result;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_result, container, false);
     }
